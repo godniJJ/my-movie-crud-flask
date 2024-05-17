e11e73f96c2d1d5e78de6c8ccbbdf43792a471c0
godniJJ <pro.hugobas@gmail.com>
2024-05-17 11:43:48 +0200

    Update test_basic.py

--- test_basic.py
+++ test_basic.py
@@ -1,35 +1,31 @@
 import os
 import unittest
- 
+
 from movie import app, db
- 
- 
+
 TEST_DB = 'test.db'
- 
 project_dir = os.path.dirname(os.path.abspath(__file__))
+
 class BasicTests(unittest.TestCase):
- 
 
     def setUp(self):
         app.config['TESTING'] = True
         app.config['WTF_CSRF_ENABLED'] = False
         app.config['DEBUG'] = False
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
-            os.path.join(project_dir, TEST_DB)
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(project_dir, TEST_DB)
         self.app = app.test_client()
-        db.drop_all()
-        db.create_all()
- 
-        self.assertEqual(app.debug, False)
- 
+        with app.app_context():
+            db.drop_all()
+            db.create_all()
+
     def tearDown(self):
-        pass
- 
- 
+        with app.app_context():
+            db.session.remove()
+            db.drop_all()
+
     def test_main_page(self):
         response = self.app.get('/', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
- 
- 
+
 if __name__ == "__main__":
     unittest.main()
